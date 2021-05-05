178224a8b9770888ba646b246478a631831f6f11
Vishvesh Patel <45102478+vishvesh193@users.noreply.github.com>
2021-05-05 22:23:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 from scipy import sparse
 import pickle
+import json
+
+from flask import request,make_response
+
 
 #Initialize the flask App
 app = Flask(__name__)
@@ -30,8 +34,8 @@ def predict():
     #int_features = [float(x) for x in request.form.values()]
     #final_features = [np.array(int_features)]
 
-    ra_ts_vect = vec_word.transform([request.form['fuel']])
-    ra_ts_vect_char = vec_char.transform([request.form['fuel']])
+    ra_ts_vect = vec_word.transform([request.form['user_input_text']])
+    ra_ts_vect_char = vec_char.transform([request.form['user_input_text']])
     ra_x_test = sparse.hstack([ra_ts_vect, ra_ts_vect_char])
 
     prediction0 = model0.predict(ra_x_test)
@@ -51,7 +55,54 @@ def predict():
     if prediction0.any() == 1 :
         output += " Toxic,"
     if prediction1.any() == 1 :
-        output += " Sever_Toxic,"
+        output += " Severe_Toxic,"
+    if prediction2.any() == 1 :
+        output += " Obscene,"
+    if prediction3.any() == 1 :
+        output += " Threat,"
+    if prediction4.any() == 1 :
+        output += " Insult,"
+    if prediction5.any() == 1 :
+        output += " Identity_Hate."
+    
+
+    if output == "" :   
+        return render_template('index.html', prediction_text='No abusive language detected')
+
+
+    return render_template('index.html', prediction_text='Categories identified :{}'.format(output))
+
+@app.route('/again')
+def again():
+    '''
+    For rendering results on HTML GUI
+    '''
+    #int_features = [float(x) for x in request.form.values()]
+    #final_features = [np.array(int_features)]
+    text = request.args.get('text')
+
+    ra_ts_vect = vec_word.transform([text])
+    ra_ts_vect_char = vec_char.transform([text])
+    ra_x_test = sparse.hstack([ra_ts_vect, ra_ts_vect_char])
+
+    prediction0 = model0.predict(ra_x_test)
+    prediction1 = model1.predict(ra_x_test)
+    prediction2 = model2.predict(ra_x_test)
+    prediction3 = model3.predict(ra_x_test)
+    prediction4 = model4.predict(ra_x_test)
+    prediction5 = model5.predict(ra_x_test)
+
+    # if(prediction0[0] + prediction1[0] + 0.6 or prediction2.any() > 0.6 or prediction3.any() > 0.6 or prediction4.any() > 0.6 or prediction5.any() > 0.6):
+    #     output = "You are bad boy!"
+    # else:
+    #     output = "You are good boy!"
+
+    output = ""
+
+    if prediction0.any() == 1 :
+        output += " Toxic,"
+    if prediction1.any() == 1 :
+        output += " Severe_Toxic,"
     if prediction2.any() == 1 :
         output += " Obscene,"
     if prediction3.any() == 1 :
@@ -61,7 +112,24 @@ def predict():
     if prediction5.any() == 1 :
         output += " Identity_Hate."
 
-    return render_template('index.html', prediction_text='Categories identified :{}'.format(output))
+    if output == "" : 
+        output="NO"
+    else :
+        output="YES"
+
+    answer=json.dumps(output)
+    res=make_response(answer)
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    res.headers['Access-Control-Allow-Headers']= "Origin, X-Requested-With, Content-Type, Accept"
+
+    return res
+    
+
+
+    
+
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
